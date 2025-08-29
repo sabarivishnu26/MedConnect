cf9352bda1fcebf0b6d131b9a0ca8873ab0489ad
sabari <sabarivishnu26@gmail.com>
2025-08-29 23:53:24 +0530

    completed login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/login' element={<Login />}></Route>
+      </Routes>
     </div>
   )
 }
